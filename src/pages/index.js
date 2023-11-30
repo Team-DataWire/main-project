6b28466af7cdd00ffbe23512cd46e9c9540fe1c9
Karthik Shankar <misterkar6@gmail.com>
2023-11-30 16:16:30 -0500

    refactor of api, adding calendar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 import { OverviewLatestOrders } from "src/sections/overview/overview-latest-orders";
 import { OverviewLatestProducts } from "src/sections/overview/overview-latest-products";
 import { OverviewSales } from "src/sections/overview/overview-sales";
@@ -14,141 +14,129 @@ import {
   Unstable_Grid2 as Grid,
 } from "@mui/material";
 import BellIcon from "@heroicons/react/24/solid/BellIcon";
-import getTopContributors from "./api/topContributors";
-import getAuthor from "./api/getAuthor";
-import getLatestPosts from "./api/latestPosts";
+import getTopContributors from "../typescript/api/topContributors";
+import getLatestUnresolved from "../typescript/api/latestUnresolved";
+import { useState, useEffect } from "react";
+import CalendarItem from "../components/calendar"
 
 export const getServerSideProps = async () => {
   const contributors = await getTopContributors();
-  const latestPosts = await getLatestPosts();
-  
+  const latestUnresolvedPosts = await getLatestUnresolved();
+
   return {
     props: {
       contributors: JSON.parse(JSON.stringify(contributors)),
-      latestPosts: JSON.parse(JSON.stringify(latestPosts)),
+      latestUnresolvedPosts: JSON.parse(JSON.stringify(latestUnresolvedPosts)),
     },
   };
 };
 
-const Page = (props) => (
-  <>
-    <Head>
-      <title>Campuswire Analytics</title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8,
-      }}
-    >
-      <Box 
-        sx={{ position: "fixed", top: 20, right: 20, zIndex: 1000 }}>
-        <Tooltip title="Urgent Questions">
-          <IconButton
-            onClick={() => {
-              alert("clicked");
-            }}
-          >
-            <Badge
-              badgeContent={2}
-              color="primary"
-              sx={{
-                "& .MuiBadge-badge": { height: 30, minWidth: 30, borderRadius: 10, fontSize: 20 },
+const Page = (props) => {
+  const [date1, setDate1] = useState(new Date(2022, 8, 5));
+  const [date2, setDate2] = useState(new Date(2022, 11, 20));
+  
+  return (
+    <>
+      <Head>
+        <title>Campuswire Analytics</title>
+      </Head>
+      <CalendarItem />
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Box sx={{ position: "fixed", top: 20, right: 20, zIndex: 1000 }}>
+          <Tooltip title="Urgent Questions">
+            <IconButton
+              onClick={() => {
+                alert("clicked");
               }}
             >
-              <SvgIcon 
-                sx={{ fontSize: 50 }}>
-                <BellIcon />
-              </SvgIcon>
-            </Badge>
-          </IconButton>
-        </Tooltip>
+              <Badge
+                badgeContent={2}
+                color="primary"
+                sx={{
+                  "& .MuiBadge-badge": { height: 30, minWidth: 30, borderRadius: 10, fontSize: 20 },
+                }}
+              >
+                <SvgIcon sx={{ fontSize: 50 }}>
+                  <BellIcon />
+                </SvgIcon>
+              </Badge>
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Container maxWidth="xl">
+          <Grid xs={6} md={6} lg={6}>
+            <OverviewLatestOrders orders={props.latestUnresolvedPosts} sx={{ height: "100%" }} />
+          </Grid>
+          <Grid xs={12} md={12} lg={8}>
+            <Grid xs={6} md={6} lg={6}>
+              <OverviewLatestOrders
+                orders={[
+                  {
+                    id: "f69f88012978187a6c12897f",
+                    category: "Homework 1",
+                    title: "Title",
+                    body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
+                  },
+                  {
+                    id: "9eaa1c7dd4433f413c308ce2",
+                    category: "Homework 2",
+                    title: "Title",
+                    body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
+                  },
+                  {
+                    id: "01a5230c811bd04996ce7c13",
+                    category: "Homework 3",
+                    title: "Title",
+                    body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
+                  },
+                  {
+                    id: "1f4e1bd0a87cea23cdb83d18",
+                    category: "Homework 4",
+                    title: "Title",
+                    body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
+                  },
+                  {
+                    id: "9f974f239d29ede969367103",
+                    category: "Homework 5",
+                    title: "Title",
+                    body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
+                  },
+                  {
+                    id: "ffc83c1560ec2f66a1c05596",
+                    category: "Homework 6",
+                    title: "Title",
+                    body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
+                  },
+                ]}
+                sx={{ height: "100%" }}
+              />
+            </Grid>
+            <Grid xs={12} md={6} lg={4}>
+              <OverviewLatestProducts products={props.contributors} sx={{ height: "100%" }} />
+            </Grid>
+            <Grid xs={9} lg={9}>
+              <OverviewSales
+                chartSeries={[
+                  {
+                    name: "Data",
+                    data: [18, 16, 5, 8, 3, 14],
+                  },
+                ]}
+                sx={{ height: "100%" }}
+              />
+            </Grid>
+          </Grid>
+        </Container>
       </Box>
-      <Container maxWidth="xl">
-        <Grid 
-          xs={6} 
-          md={6} 
-          lg={6}>
-          <OverviewLatestOrders orders={props.latestPosts} 
-            sx={{ height: "100%" }} />
-        </Grid>
-        <Grid 
-          xs={12} 
-          md={12} 
-          lg={8}>
-
-          <Grid 
-            xs={6} 
-            md={6} 
-            lg={6}>
-            <OverviewLatestOrders
-              orders={[
-                {
-                  id: "f69f88012978187a6c12897f",
-                  category: "Homework 1",
-                  title: "Title",
-                  body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
-                },
-                {
-                  id: "9eaa1c7dd4433f413c308ce2",
-                  category: "Homework 2",
-                  title: "Title",
-                  body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
-                },
-                {
-                  id: "01a5230c811bd04996ce7c13",
-                  category: "Homework 3",
-                  title: "Title",
-                  body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
-                },
-                {
-                  id: "1f4e1bd0a87cea23cdb83d18",
-                  category: "Homework 4",
-                  title: "Title",
-                  body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
-                },
-                {
-                  id: "9f974f239d29ede969367103",
-                  category: "Homework 5",
-                  title: "Title",
-                  body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
-                },
-                {
-                  id: "ffc83c1560ec2f66a1c05596",
-                  category: "Homework 6",
-                  title: "Title",
-                  body: "abcabacacacacac abcabacacacacac abcabacacacacac abcabacacacacac",
-                },
-              ]}
-              sx={{ height: "100%" }}
-            />
-          </Grid>
-          <Grid 
-            xs={12} 
-            md={6} 
-            lg={4}>
-            <OverviewLatestProducts products={props.contributors} 
-              sx={{ height: "100%" }} />
-          </Grid>
-          <Grid 
-            xs={9} 
-            lg={9}>
-            <OverviewSales
-              chartSeries={[
-                {
-                  name: "Data",
-                  data: [18, 16, 5, 8, 3, 14],
-                },
-              ]}
-              sx={{ height: "100%" }}
-            />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-);
+    </>
+  );
+};
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
---
+++ src/typescript/categories.ts
@@ -0,0 +1,12 @@
+const data: { [key: string]: string } = {
+  "c1741f16-5771-4c94-8055-9738b74aca9f": "Course Logistics",
+  "e03d79c7-74a5-4db3-8e43-45b4c5335051": "HW 1",
+  "b0253115-7d59-4026-adaf-6375c1305456": "HW 2",
+  "29456601-2678-4c68-bac8-cb083d6a18f5": "HW 3",
+  "29a0efef-4c5e-43b0-979b-76077929b4a4": "HW 4",
+  "9a7fa05a-6ad7-4423-8a99-7c43713fe6b7": "HW 5",
+  "fa89385b-0384-4274-91f4-e56c97171be1": "HW 6",
+  "99a4beac-edfa-4bc4-8ceb-de5ed823e6a3": "Final Exam",
+};
+
+export default data;
